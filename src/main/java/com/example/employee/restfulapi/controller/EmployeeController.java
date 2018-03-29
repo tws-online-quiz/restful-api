1c3b3b9cd100f5768a62b1d839439fc3578d0db2
DQing <qqdou@thoughtworks.com>
2018-03-29 21:02:46 +0800

    finish function api

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -1,29 +1,74 @@
-//package com.example.employee.restfulapi.controller;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//@Controller
-//@RequestMapping(value = "/")
-//public class EmployeeController {
-//
-//
-//    @PostMapping(value = "/registerState")
-//    ResponseEntity save(@RequestBody Map<String, String> registerState) throws Exception {
-//
-//        String address = registerState.get("address");
-//
-//        Map<String, String> result = new HashMap<>();
-//        result.put("step", "2");
-//        return new ResponseEntity<>(result, HttpStatus.OK);
-//    }
-//}
-//
-//
+package com.example.employee.restfulapi.controller;
+
+import com.example.employee.restfulapi.entity.Employee;
+import com.example.employee.restfulapi.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@Controller
+@RequestMapping(value = "/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping(value = "")
+    ResponseEntity getEmployees() throws Exception {
+        List<Employee> employeeList = employeeRepository.findAll();
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity getEmployeeById(@PathVariable long id) throws Exception {
+        Employee employee = employeeRepository.findOne(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/page/{page}/pageSize/{pageSize}")
+    public ResponseEntity getEmployeePage(@PathVariable int page, @PathVariable int pageSize) throws Exception {
+
+        List<Employee> employeeList = employeeRepository.findAll();
+        List<Employee> result = new ArrayList<>();
+        int count = employeeRepository.findAll().size();
+        int startIndex = (page - 1) * pageSize;
+        int endIndex = page * pageSize;
+        for (int i = startIndex; i < endIndex; i++) {
+            result.add(employeeList.get(i));
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "")
+    public ResponseEntity addCompany(@RequestBody Map<String, String> employeeInfo) throws Exception {
+        String name = employeeInfo.get("name");
+        int age = Integer.parseInt(employeeInfo.get("age"));
+        String gender = employeeInfo.get("gender");
+        int salary = Integer.parseInt(employeeInfo.get("salary"));
+        employeeRepository.save(new Employee(name, age, gender, salary));
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity updateCompany(@PathVariable long id, @RequestBody Map<String, String> employeeInfo) throws Exception {
+        int age = Integer.parseInt(employeeInfo.get("age"));
+        String gender = employeeInfo.get("gender");
+        Employee employee = employeeRepository.findOne(id);
+        employee.setAge(age);
+        employee.setGender(gender);
+        employeeRepository.save(employee);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity deleteCompany(@PathVariable long id) throws Exception {
+        employeeRepository.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
+
+
