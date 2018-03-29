277abd2a88f04299104a7d2476ee23c1fd73cef0
DQing <qqdou@thoughtworks.com>
2018-03-29 17:20:07 +0800

    update sql

--- src/main/resources/db/migration/V2__Create_table_Employee.sql
+++ src/main/resources/db/migration/V2__Create_table_Employee.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Employee(
     id INT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    age INT NOT NULL,
+    name VARCHAR(255),
+    age INT,
     gender VARCHAR(10),
-    companyId INT NOT NULL,
-    salary INT NOT NULL,
+    companyId INT(11),
+    salary INT,
     constraint `company_id_for` FOREIGN KEY (`companyId`) REFERENCES `Company` (`id`) on delete cascade on update cascade
 ) engine=InnoDB DEFAULT CHARSET = utf8;
 
