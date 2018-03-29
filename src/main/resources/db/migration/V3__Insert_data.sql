af1e8c5e3c74dc294ae1df398ea767ac29dc8793
DQing <qqdou@thoughtworks.com>
2018-03-29 13:19:12 +0800

    add data to database
    Signed-off-by: DQing <qqdou@thoughtworks.com>

--- src/main/resources/db/migration/V3__Insert_data.sql
+++ src/main/resources/db/migration/V3__Insert_data.sql
@@ -2,23 +2,28 @@ INSERT INTO Company (id,companyName,employeesNumber) VALUES ( 1, 'baidu',1000 );
 INSERT INTO Company (id,companyName,employeesNumber) VALUES ( 2, 'alibaba',200 );
 INSERT INTO Company (id,companyName,employeesNumber) VALUES ( 3, 'ThoughtWorks',3000 );
 INSERT INTO Company (id,companyName,employeesNumber) VALUES ( 4, 'tengxun',3000 );
-INSERT INTO Company (id,companyName,employeesNumber) VALUES ( 5, 'tengxun',3000 );
-INSERT INTO Company (id,companyName,employeesNumber) VALUES ( 6, 'tengxun',3000 );
-INSERT INTO Company (id,companyName,employeesNumber) VALUES ( 7, 'tengxun',3000 );
-INSERT INTO Company (id,companyName,employeesNumber) VALUES ( 8, 'tengxun',3000 );
-INSERT INTO Company (id,companyName,employeesNumber) VALUES ( 9, 'tengxun',3000 );
-INSERT INTO Company (id,companyName,employeesNumber) VALUES ( 10, 'tengxun',3000 );
+INSERT INTO Company (id,companyName,employeesNumber) VALUES ( 5, 'huiwei',3000 );
 
-INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 1, 'xiaoming',20,'male',1,6000 );
-INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 2, 'xiaohong',19,'female',1,7000 );
-INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 3, 'xiaozhi',19,'male',1,8000 );
-INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 4, 'xiaogang',19,'male',1,6000 );
-INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 5, 'xiaoxia',19,'female',1,4000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 1, 'baidu1',20,'male',1,6000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 2, 'baidu2',19,'female',1,7000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 3, 'baidu3',19,'male',1,8000 );
 
-INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 6, 'xiaoming',20,'male',2,6000 );
-INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 7, 'xiaohong',19,'female',2,7000 );
-INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 8, 'xiaozhi',19,'male',2,8000 );
-INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 9, 'xiaogang',19,'male',2,6000 );
-INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 10, 'xiaoxia',19,'female',2,4000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 4, 'alibaba1',20,'male',2,6000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 5, 'alibaba2',19,'female',2,7000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 6, 'alibaba3',19,'male',2,8000 );
+
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 7, 'ThoughtWorks1',20,'male',2,6000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 8, 'ThoughtWorks2',19,'female',2,7000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 9, 'ThoughtWorks3',19,'male',2,8000 );
+
+
+
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 10, 'tengxun1',20,'male',2,6000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 11, 'tengxun2',19,'female',2,7000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 12, 'tengxun3',19,'male',2,8000 );
+
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 13, 'huiwei1',20,'male',2,6000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 14, 'huiwei2',19,'female',2,7000 );
+INSERT INTO Employee (id,name,age,gender,companyId,salary) VALUES ( 15, 'huiwei3',19,'male',2,8000 );
 
 
